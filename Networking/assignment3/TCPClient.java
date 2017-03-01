5c871025088e8a5e0b92a6f72f7b86eacaef9047
Aurghyadip Kundu <adkundu@gmail.com>
2017-03-01 20:41:16 +0530

    proper comments and exceptions added

--- Networking/assignment3/TCPClient.java
+++ Networking/assignment3/TCPClient.java
@@ -3,16 +3,20 @@ import java.net.*;
 
 class TCPClient {
 	public static void main(String args[]) throws IOException {
-	String sentence;
-	String modifiedSentence;
-	BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-	Socket cs = new Socket("localhost",8080);
-	DataOutputStream outToServer = new DataOutputStream(cs.getOutputStream());
-	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-	sentence = inFromUser.readLine();
-	outToServer.writeBytes(sentence + '\n');
-	modifiedSentence = inFromServer.readLine();
-	System.out.println("SERVER OP " + modifiedSentence);
-	cs.close();
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			Socket cs = new Socket("localhost",1234); // This can throw exceptions
+			DataOutputStream outToServer = new DataOutputStream(cs.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			sentence = inFromUser.readLine();
+			outToServer.writeBytes(sentence + '\n');
+			modifiedSentence = inFromServer.readLine();
+			System.out.println("Output from Server : " + modifiedSentence);
+			cs.close();
+		} catch (ConnectException e) {
+			System.err.println("Exception:" + "\n" + e.getMessage());
+		}
 	}
 }
--- Networking/assignment3/TCPServer.java
+++ Networking/assignment3/TCPServer.java
@@ -5,18 +5,30 @@ class TCPServer {
 	public static void main(String args[]) throws IOException {
 		String clientSentence ;
 		String capitalizedSentence;
-		ServerSocket ws = new ServerSocket(8080);
-		while(true)
+		ServerSocket ws = new ServerSocket(1234);
+		ws.setSoTimeout(15000); // Setting the timeout time to 15 seconds.
+
+		boolean connection = true; // Connection flag.
+
+		while(connection)
 		{
-			Socket cs = ws.accept();
-			System.out.println("Connected to Client");
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(cs.getOutputStream());
-			
-			clientSentence = inFromClient.readLine();
-			capitalizedSentence = clientSentence.toUpperCase() + "\n";
-			outToClient.writeBytes(capitalizedSentence);
-			System.out.println("Msg sent");
+			try {
+				Socket cs = ws.accept();
+				System.out.println("Connected to Client"); // Log if connected.
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+				DataOutputStream outToClient = new DataOutputStream(cs.getOutputStream());
+				clientSentence = inFromClient.readLine();
+				capitalizedSentence = clientSentence.toUpperCase() + "\n";
+				outToClient.writeBytes(capitalizedSentence);
+				/*
+				// Logging if the last line is actually executing
+				// in case of a fault.
+				System.out.println("Msg sent");
+				*/
+			} catch (SocketTimeoutException e) {
+				System.err.println("Server timed out." + "\n" + e.getMessage());
+				connection = false;
+			}
 		}
 	}
 } 
