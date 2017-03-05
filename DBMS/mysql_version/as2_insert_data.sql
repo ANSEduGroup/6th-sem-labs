205cd593f2860802a67f4ea5809ed540a3b8bc55
Aurghyadip <adkundu@gmail.com>
2017-03-05 21:38:54 +0530

    edited files as per new assignments

--- DBMS/mysql_version/as2_insert_data.sql
+++ DBMS/mysql_version/as2_insert_data.sql
@@ -1,63 +1,98 @@
-# Setting the foreign keys
+-- Create tables
+
+-- Create table hotel
+CREATE TABLE hotel_006 (
+  hotel_no VARCHAR(10) PRIMARY KEY,
+  name VARCHAR(20),
+  address VARCHAR(30)
+);
+
+-- Create table room
+CREATE TABLE room_006 (
+  room_no VARCHAR(10),
+  hotel_no VARCHAR(10),
+  type VARCHAR(10),
+  price BIGINT,
+  PRIMARY KEY(room_no, hotel_no)
+);
+
+-- Create table guests
+CREATE TABLE guest_006 (
+  guest_no VARCHAR(10) PRIMARY KEY,
+  name VARCHAR(30),
+  address VARCHAR(30),
+  age BIGINT
+);
+
+-- Create table booking
+CREATE TABLE booking_006 (
+  room_no VARCHAR(10),
+  hotel_no VARCHAR(10),
+  guest_no VARCHAR(10),
+  date_from DATETIME,
+  date_to DATETIME,
+  PRIMARY KEY(hotel_no, guest_no, date_from)
+);
+
+-- Add foreign key constraints to the tables
+
+-- For table room, constraint is hotel_key, hotel_no is the foreign key
+-- Reference hotel table
 ALTER TABLE room_006
-ADD CONSTRAINT hotel_key
+ADD CONSTRAINT hotel_key_006
 FOREIGN KEY (hotel_no) REFERENCES hotel_006(hotel_no);
 
+-- For table booking, constraint is room_key, room_no and hotel_no are both foreign keys
+-- Reference room table
 ALTER TABLE booking_006
-ADD CONSTRAINT room_key
+ADD CONSTRAINT room_key_006
 FOREIGN KEY (room_no, hotel_no) REFERENCES room_006(room_no, hotel_no);
 
+-- For table booking, constraint is guest_key, guest_no is the foreign key
+-- Reference guest table
 ALTER TABLE booking_006
-ADD CONSTRAINT guest_key
+ADD CONSTRAINT guest_key_006
 FOREIGN KEY (guest_no) REFERENCES guest_006(guest_no);
 
+-- Insert records in tables
 
-# Insert into hotel
+-- Insert into hotel
+INSERT INTO HOTEL_006 VALUES ('H1','GRAND','KOLKATA');
+INSERT INTO HOTEL_006 VALUES ('H2','TAJ','MUMBAI');
+INSERT INTO HOTEL_006 VALUES ('H3','PURI','PURI');
+INSERT INTO HOTEL_006 VALUES ('H4','ITC','KOLKATA');
+INSERT INTO HOTEL_006 VALUES ('H5','PARK','CHENNAI');
+INSERT INTO HOTEL_006 VALUES ('H6','ITC','MUMBAI');
+INSERT INTO HOTEL_006 VALUES ('H7','SONALI','PURI');
 
-INSERT INTO hotel_006 (hotel_no, name, address) VALUES
-   ('H1', 'PURI', 'PURI'), 
-   ('H2','TAJ', 'MUMBAI'), 
-   ('H3', 'SONALI', 'PURI'), 
-   ('H4', 'ITC', 'KOLKATA'), 
-   ('H5', 'PARK', 'CHENNAI'), 
-   ('H6', 'SAMUDRA', 'CHENNAI');
+-- Insert into room
+INSERT INTO ROOM_006 VALUES ('R1','H1','AC',2000);
+INSERT INTO ROOM_006 VALUES ('R2','H1','NONAC',1000);
+INSERT INTO ROOM_006 VALUES ('R1','H2','DELUX',2500);
+INSERT INTO ROOM_006 VALUES ('R2','H2','NONAC',1000);
+INSERT INTO ROOM_006 VALUES ('R3','H4','AC',1800);
+INSERT INTO ROOM_006 VALUES ('R4','H5','DELUX',3000);
+INSERT INTO ROOM_006 VALUES ('R2','H3','AC',2900);
+INSERT INTO ROOM_006 VALUES ('R1','H6','AC',3500);
+INSERT INTO ROOM_006 VALUES ('R5','H7','NONAC',900);
 
-# Insert into room
+-- Insert into guest
+INSERT INTO guest_006 VALUES ('G1', 'RAM', 'MUMBAI', '43');
+INSERT INTO guest_006 VALUES ('G2', 'SHYAM', 'KOLKATA', '32');
+INSERT INTO guest_006 VALUES ('G3', 'JADU', 'KOLKATA', '26');
+INSERT INTO guest_006 VALUES ('G4', 'MADHU', 'KOLKATA', '48');
+INSERT INTO guest_006 VALUES ('G5', 'SMITH', 'CHENNAI', '55');
+INSERT INTO guest_006 VALUES ('G6', 'JONES', 'CHENNAI', '39');
+INSERT INTO guest_006 VALUES ('G7', 'RIMI', 'KOLKATA', '38');
+INSERT INTO guest_006 VALUES ('G8', 'RAJA', 'PURI', '24');
+INSERT INTO guest_006 VALUES ('G9', 'SIMA', 'MUMBAI', '62');
 
-INSERT INTO room_006 (room_no, hotel_no, type, price) VALUES
-   ('R1', 'H1', 'AC', '2000'),
-   ('R2', 'H1', 'NONAC', '1000'),
-   ('R1', 'H2', 'DELUX', '2500'),
-   ('R2', 'H2', 'NONAC', '1000'),
-   ('R2', 'H3', 'AC', '3900'),
-   ('R3', 'H4', 'AC', '1800'),
-   ('R4', 'H5', 'DELUX', '4000'),
-   ('R1', 'H6', 'AC', '4000'),
-   ('R2', 'H6', 'NONAC', '1500');
-
-# Insert into guest
-
-INSERT INTO guest_006 (guest_no, name, address) VALUES 
-   ('G1', 'RAJA', 'MUMBAI'),
-   ('G2', 'SIMA', 'KOLKATA'), 
-   ('G3', 'RAHUL', 'KOLKATA'), 
-   ('G4', 'MILI', 'KOLKATA'),
-   ('G5', 'SMITH', 'CHENNAI'),
-   ('G6', 'JONES', 'CHENNAI'), 
-   ('G7', 'RIMI', 'KOLKATA'),
-   ('G8', 'REKHA', 'CHENNAI'), 
-   ('G9', 'RISHI', 'PURI');
-
-# Insert into booking
-
-INSERT INTO booking_006 (room_no, hotel_no, guest_no, date_from, date_to) VALUES
-  ('R1','H1', 'G1', '2016-01-01','2016-01-08'),
-  ('R2', 'H1', 'G2', '2016-01-12', '2016-01-18'),
-  ('R1','H2', 'G3', '2016-01-01', '2016-01-08'),
-  ('R2', 'H2', 'G4', '2016-01-18', '2016-01-28'),
-  ('R3','H4', 'G5', '2016-01-10', '2016-01-18'),
-  ('R4', 'H5', 'G6', '2016-01-11', '2016-01-18'),
-  ('R2','H3', 'G7', '2016-01-21', '2016-01-28'),
-  ('R1', 'H6', 'G8', '2016-01-12', '2016-01-20'),
-  ('R2', 'H6', 'G9', '2016-01-21', '2017-01-28');
-
+-- Insert into booking
+INSERT INTO booking_006 VALUES ('R1','H1', 'G1', '2016-01-01','2016-01-08');
+INSERT INTO booking_006 VALUES ('R2', 'H1', 'G2', '2016-01-12', '2016-01-18');
+INSERT INTO booking_006 VALUES ('R1','H2', 'G3', '2016-02-01', '2016-02-08');
+INSERT INTO booking_006 VALUES ('R2', 'H2', 'G4', '2016-01-18', '2016-01-28');
+INSERT INTO booking_006 VALUES ('R3','H4', 'G5', '2016-03-10', '2016-03-18');
+INSERT INTO booking_006 VALUES ('R4', 'H5', 'G6', '2016-01-11', '2016-01-18');
+INSERT INTO booking_006 VALUES ('R2','H3', 'G7', '2016-02-21', '2016-02-28');
+INSERT INTO booking_006 VALUES ('R1','H6', 'G8', '2016-02-22', '2016-02-27');
--- DBMS/mysql_version/assignment3.sql
+++ DBMS/mysql_version/assignment3.sql
@@ -1,48 +1,67 @@
-# Find out the number of hotels
-select count(distinct hotel_no) from hotel_006;
+-- Find out the number of hotels
+select count(distinct hotel_no) as "No of Hotels" from hotel_006;
 
-# Average price of a room
-select avg(price) from room_006;
-# Grouped by hotel no
-# select hotel_no, avg(price) from room_006 GROUP BY hotel_no;
+-- Average price of a room
+select avg(price) as "Average Price of a Room" from room_006;
+-- Grouped by hotel no
+-- select hotel_no, avg(price) from room_006 GROUP BY hotel_no;
 
-# No of different guests booked for january only
-select count(distinct guest_no) from booking_006 where month(date_from) = 1 and month(date_to) = 1;
+-- No of different guests booked for january only
+select count(distinct guest_no) from booking_006
+where month(date_from) = 1
+and month(date_to) = 1;
 
-# Room details for hotel ITC
+-- Minimum and maximum price of a room
+select min(price) as "Min Price", max(price) as "Max Price"
+from room_006;
+
+-- Average age of a guest
+select avg(age) as "Average Age" from guest_006;
+
+-- List guest names of youngest and oldest. 
+select guest_006.name, guest_006.age from guest_006
+where guest_006.age = (select min(age) from guest_006)
+or guest_006.age = (select max(age) from guest_006);
+
+-- Room details for hotel ITC
 select room_no, type, price from room_006
 where hotel_no = (select hotel_no from hotel_006 where name = 'ITC');
 
-# List all the guest names of PURI Hotel
+-- List all the guest names of PURI Hotel
 select booking_006.room_no, guest_006.name from guest_006, hotel_006, booking_006
 where booking_006.hotel_no = hotel_006.hotel_no 
 and guest_006.guest_no = booking_006.guest_no
 and hotel_006.name = 'PURI';
 
-# List the hotel name where Jadu/Rahul Stays
+-- List the hotel name where Jadu/Rahul Stays
 select * from booking_006, guest_006
 where booking_006.guest_no = guest_006.guest_no
-and guest_006.name = 'RAHUL';
+and guest_006.name = 'JADU';
 
-# List the guest names whose address and hotel address are same
+-- List the guest names whose address and hotel address are same
 select guest_006.name from guest_006, booking_006, hotel_006
 where guest_006.guest_no = booking_006.guest_no
 and booking_006.hotel_no = hotel_006.hotel_no
 and guest_006.address = hotel_006.address;
 
-# List the number of rooms in each hotel
+-- List the number of rooms in each hotel
 select count(room_no), hotel_no, avg(price) FROM room_006 GROUP BY hotel_no;
 
-# List the number of rooms in each hotel in KOLKATA
-select count(room_no), room_006.hotel_no, avg(price) FROM room_006, hotel_006
+-- List the number of rooms in each hotel in KOLKATA
+select count(room_006.room_no) as "Rooms", hotel_006.name FROM room_006, hotel_006
 WHERE room_006.hotel_no = hotel_006.hotel_no
-and hotel_006.address = 'KOLKATA';
+and hotel_006.address = 'KOLKATA' group by hotel_006.name;
 
-# Delete the booking details of the guest with guest with guest_no G4
-DELETE from booking_006 WHERE guest_no = 'G4';
+-- List the maximum price in each hotel
+select hotel_006.hotel_no, max(price) from room_006, hotel_006
+where room_006.hotel_no = hotel_006.hotel_no
+group by hotel_006.hotel_no order by hotel_006.hotel_no;
 
-# Update hotel prices by 5%
-UPDATE room_006 set price = (price * 1.05);
+-- -- Delete the booking details of the guest with guest with guest_no G4
+-- DELETE from booking_006 WHERE guest_no = 'G4';
 
-# Update the guest name to JISHU where the guest_no is G3
-update guest_006 set name = 'JISHU' where guest_no = 'G3';
+-- -- Update hotel prices by 5%
+-- UPDATE room_006 set price = (price * 1.05);
+
+-- -- Update the guest name to JISHU where the guest_no is G3
+-- update guest_006 set name = 'JISHU' where guest_no = 'G3';
