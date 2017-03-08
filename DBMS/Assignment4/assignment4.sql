74c65802ed5a324f0a7231d9bc8db753b1218d9c
Aurghyadip Kundu <adkundu@gmail.com>
2017-03-08 22:45:35 +0530

    Added the new assignment contents

--- DBMS/Assignment4/assignment4.sql
+++ DBMS/Assignment4/assignment4.sql
@@ -1,34 +1,66 @@
 -- Order by
 
+--1
 select * from hotel_006 order by name desc;
 
+--2
 select * from room_006 order by price asc;
 
+--3
 select * from guest_006 order by name desc;
+
+--4
+select * from guest_006 order by age asc;
 
 -- Pattern Matching
 
+--1
 select name from guest_006
 where name like 'R%';
 
+--2
 select name from guest_006
 where name like '_O%' or
 name like '_H%';
 
+--3
 select name from guest_006
 where name like 'J___';
 
+--4
+select * from hotel_006
+where name like '__R%';
+
+--5
+select name from guest_006
+where name like '%U' or
+name like '%A';
+
 -- Range Searching
 
+--1
 select * from room_006
 where price between 2000 and 3000;
 
+--2
 select guest_006.name, booking_006.date_from from booking_006, guest_006
 where guest_006.guest_no = booking_006.guest_no
 and date_from between '01-JAN-2016' and '10-JAN-2016';
 
+--3
+select name, age from guest_006
+where age between 30 and 45;
+
+-- IN and NOT IN
+
+--1
 select name from guest_006
 where name in ('RAM','SMITH','JONES');
 
+--2
 select * from hotel_006
 where name not in ('PURI','TAJ');
+
+--3
+select * from guest_006
+where address in ('KOLKATA', 'CHENNAI');
--- DBMS/Assignment5/assignment5.sql
+++ DBMS/Assignment5/assignment5.sql
@@ -1,3 +1,33 @@
+-- Check Constraint
+-- select constraint_name, constraint_type, table_name from user_constraints;
+
+-- 1
+alter table room_006
+add constraint price_check_006 check (price >= 100);
+
+-- 2
+alter table booking_006
+add constraint date_check_006 check (date_from < date_to);
+
+--3
+alter table guest_006
+add constraint age_check_006 check (age >= 18);
+
+-- TO_CHAR
+
+--1
+select
+  guest_006.name,
+  to_char(booking_006.date_from, 'DD/MM/YYYY') as "Date From",
+  to_char(booking_006.date_to, 'DD/MM/YYYY') as "Date To"
+  from booking_006, guest_006
+where booking_006.guest_no = guest_006.guest_no;
+
+--2
+
+
+-- Subquery
+
 -- Price of the room is more than the averge price of other rooms
 select * from room_006
 where price > (select avg(price) from room_006);
@@ -16,3 +46,14 @@ where ADDRESS = (SELECT ADDRESS from HOTEL_006 WHERE name = 'GRAND');
 select * from guest_006
 where address = (select address from guest_006 where name = 'SHYAM');
 
+-- 5
+select * from guest_006
+where age < (select age from guest_006 where name = 'RAM');
+
+-- 6
+select * from guest_006
+where age > (select avg(age) from guest_006);
+
+-- 7
+select * from guest_006
+where age < (select avg(age) from guest_006);
