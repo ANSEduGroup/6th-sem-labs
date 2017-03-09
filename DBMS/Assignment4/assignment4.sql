e6da56b84f25fb4c3692dd4140b538bcc452e01b
Aurghyadip Kundu <adkundu@gmail.com>
2017-03-09 18:36:56 +0530

    added proper comments

--- DBMS/Assignment4/assignment4.sql
+++ DBMS/Assignment4/assignment4.sql
@@ -1,66 +1,69 @@
+set linesize 100;
+set pagesize 200;
+
 -- Order by
 
---1
+-- List hotel names in descending order of names
 select * from hotel_006 order by name desc;
 
---2
+-- List the rooms in ascending order of prices
 select * from room_006 order by price asc;
 
---3
+-- List the guest details in descending order of names
 select * from guest_006 order by name desc;
 
---4
+-- List the guest names in ascending order of their ages
 select * from guest_006 order by age asc;
 
 -- Pattern Matching
 
---1
+-- Select the guests whose names start with 'R'
 select name from guest_006
 where name like 'R%';
 
---2
+-- Select the guests whose names have 2nd character 'O' or 'H'
 select name from guest_006
 where name like '_O%' or
 name like '_H%';
 
---3
+-- Select the guests whose name begins with 'J' and a 4 letter word
 select name from guest_006
 where name like 'J___';
 
---4
+-- Select the hotels whose address has 'R' as the 3rd character
 select * from hotel_006
 where name like '__R%';
 
---5
+-- List the guest names whose names end either with 'U' or 'A'
 select name from guest_006
 where name like '%U' or
 name like '%A';
 
 -- Range Searching
 
---1
+-- List the room with price between 2000 and 3000
 select * from room_006
 where price between 2000 and 3000;
 
---2
+-- Select the guest names who booked hotel from 1st Jan to 10th Jan
 select guest_006.name, booking_006.date_from from booking_006, guest_006
 where guest_006.guest_no = booking_006.guest_no
 and date_from between '01-JAN-2016' and '10-JAN-2016';
 
---3
+-- List the guest names and ages whose age is between 30 and 45
 select name, age from guest_006
 where age between 30 and 45;
 
 -- IN and NOT IN
 
---1
+-- Select the guest named RAM, SMITH, JONES
 select name from guest_006
 where name in ('RAM','SMITH','JONES');
 
---2
+-- Select the hotel other than PURI, TAJ
 select * from hotel_006
 where name not in ('PURI','TAJ');
 
---3
+-- Select the guest whose address is KOLKATA, CHENNAI
 select * from guest_006
 where address in ('KOLKATA', 'CHENNAI');
--- DBMS/Assignment5/assignment5.sql
+++ DBMS/Assignment5/assignment5.sql
@@ -1,30 +1,48 @@
 -- Check Constraint
--- select constraint_name, constraint_type, table_name from user_constraints;
+/*
+select
+  constraint_name as "Name",
+  constraint_type as "Type",
+  table_name as "Table"
+from user_constraints
+where
+  table_name = 'HOTEL_006' or
+  table_name = 'ROOM_006' or
+  table_name = 'BOOKING_006' or
+  table_name = 'GUEST_006';
+*/
+set linesize 100;
+set pagesize 200;
 
--- 1
+-- Alter table room with check constraint price >= 100
 alter table room_006
 add constraint price_check_006 check (price >= 100);
 
--- 2
+-- Check constraint date from < date to
 alter table booking_006
 add constraint date_check_006 check (date_from < date_to);
 
---3
+-- guest age > 18
 alter table guest_006
 add constraint age_check_006 check (age >= 18);
 
 -- TO_CHAR
 
---1
+-- Date to and date from in the form of  (DD/MM/YYYY)
 select
-  guest_006.name,
+  guest_006.name as "Guest Name",
   to_char(booking_006.date_from, 'DD/MM/YYYY') as "Date From",
   to_char(booking_006.date_to, 'DD/MM/YYYY') as "Date To"
   from booking_006, guest_006
 where booking_006.guest_no = guest_006.guest_no;
 
---2
-
+-- Price in the format (9,999.99)
+select
+  room_no as "Room No",
+  hotel_no as "Hotel No",
+  type as "Type",
+  to_char(price, '9,999.99')
+from room_006;
 
 -- Subquery
 
@@ -46,14 +64,14 @@ where ADDRESS = (SELECT ADDRESS from HOTEL_006 WHERE name = 'GRAND');
 select * from guest_006
 where address = (select address from guest_006 where name = 'SHYAM');
 
--- 5
+-- Guests who are younger than RAM
 select * from guest_006
 where age < (select age from guest_006 where name = 'RAM');
 
--- 6
+-- Guests whose age is more than avg age of all guests
 select * from guest_006
 where age > (select avg(age) from guest_006);
 
--- 7
+-- Guests whose age is less than avg age of all guests
 select * from guest_006
 where age < (select avg(age) from guest_006);
